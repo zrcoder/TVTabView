ff4367ee4efb2b715228d410ccd67350f9fdd7c3
admin <love-nankai@163.com>
2016-08-29 10:52:51 +0800

    ~

--- Source/TVTabView.h
+++ Source/TVTabView.h
@@ -22,54 +22,28 @@ typedef void(^TVAction)();
 
 @interface TVTabView : UIView
 
-/**
- *  Default is 44
- */
-@property (nonatomic, assign) CGFloat tabHeight;
+@property (nonatomic, assign) CGFloat tabHeight;    //  Default is 44
+
+@property (nonatomic, assign) CGFloat bodyTopMargin;    //  Default is 20
+
+@property (nonatomic, assign) CGFloat bodyBottomMargin;    //  Default is 20
+
+@property (nonatomic, copy) UIColor *tabBackGroundColor;    //  Default is nil
+
+@property (nonatomic, assign) CGFloat tabFontSize;    //  Default is 14
+
+@property (nonatomic, copy) UIColor *titleColor;    //  Default is black (red:51 green:51 blue: 51 --#333333)
+
+@property (nonatomic, copy) UIColor *selectedTitleColor;    //  Default is green (red:33 green:149 blue: 128 --#219580)
+
+@property (nonatomic, copy) UIColor *tabLineColor;    //  Default is nil
+
+@property (nonatomic, copy) UIColor *selectedTabLineColor;    //  Default is the same as selectedTitleColor default - green (red:33 green:149 blue:128 --#219580)
 
 /**
- *  Default is 20
- */
-@property (nonatomic, assign) CGFloat bodyTopMargin;
-
-/**
- *  Default is 20
- */
-@property (nonatomic, assign) CGFloat bodyBottomMargin;
-
-/**
- *  Default is nil
- */
-@property (nonatomic, copy) UIColor *tabBackGroundColor;
-
-/**
- *  Default is 14
- */
-@property (nonatomic, assign) CGFloat tabFontSize;
-
-/**
- *  Default is black (red:51 green:51 blue: 51 --#333333)
- */
-@property (nonatomic, copy) UIColor *titleColor;
-/**
- *  Default is green (red:33 green:149 blue: 128 --#219580)
- */
-@property (nonatomic, copy) UIColor *selectedTitleColor;
-
-/**
- *  Default is nil
- */
-@property (nonatomic, copy) UIColor *tabLineColor;
-/**
- *  Default is the same as selectedTitleColor default - green (red:33 green:149 blue:128 --#219580)
- */
-@property (nonatomic, copy) UIColor *selectedTabLineColor;
-
-/**
- *  After setted, we will begin to build subviews(buttons, and views in body)
+ *  After setted, we will begin to build subviews(buttons, or views in body)
  */
 @property (nonatomic, strong) NSArray<TVItem *> * items;
-
 
 /**
  *  If you set it, the buttons and views in the body will scroll autollicly.
